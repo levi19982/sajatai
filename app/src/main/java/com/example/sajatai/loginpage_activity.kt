f7a595abcdf84c7f4babff0a1655d65dced43853
Levente <leventevass19982@gmail.com>
2023-10-17 23:50:01 +0200

    signupUI

--- app/src/main/java/com/example/sajatai/loginpage_activity.kt
+++ app/src/main/java/com/example/sajatai/loginpage_activity.kt
@@ -1,11 +1,20 @@
 package com.example.sajatai
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class loginpage_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginpage)
+
+        val signupbutton = findViewById<TextView>(R.id.sign_up)
+
+        signupbutton.setOnClickListener{
+            val tosignup = Intent(this@loginpage_activity, signup_activity::class.java)
+            startActivity(tosignup)
+        }
     }
 }
---
+++ app/src/main/java/com/example/sajatai/signup_activity.kt
@@ -0,0 +1,12 @@
+package com.example.sajatai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class signup_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
