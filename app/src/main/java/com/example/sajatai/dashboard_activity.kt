0d7dd558294ab3dada223ddbeb3479f80b1c06e9
Levente <leventevass19982@gmail.com>
2023-10-18 09:59:18 +0200

    dashboard minimal design

--- app/src/main/java/com/example/sajatai/dashboard_activity.kt
+++ app/src/main/java/com/example/sajatai/dashboard_activity.kt
@@ -2,10 +2,15 @@ package com.example.sajatai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class dashboard_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val userName = intent.getStringExtra("USERNAME")
+        val userNameTextView = findViewById<TextView>(R.id.nev_id)
+        userNameTextView.text = userName
     }
 }
--- app/src/main/java/com/example/sajatai/loginpage_activity.kt
+++ app/src/main/java/com/example/sajatai/loginpage_activity.kt
@@ -9,8 +9,12 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import org.mindrot.jbcrypt.BCrypt
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
@@ -44,9 +48,20 @@ class loginpage_activity : AppCompatActivity() {
                 val storedHashedPassword = user["password"] ?: return@addOnSuccessListener
 
                 if (BCrypt.checkpw(password, storedHashedPassword)) {
-                    // Login successful
-                    Toast.makeText(this, "Sikeres bejelentkezés", Toast.LENGTH_SHORT).show()
-                    // Navigate to the main activity or dashboard
+                    val hashedEmail = hashString(encrypt(email, secretKey))
+                    val userRef = database.child("users").child(hashedEmail)
+                    userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            val user = snapshot.getValue<Map<String, Any>>()
+                            val encryptedName = user?.get("name") as? String ?: "No Name"
+                            val userName = decrypt(encryptedName, "s3cr3tK3yF0rA3S!")
+                            navigateToDashboard(userName)
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            // Handle error
+                        }
+                    })
                 } else {
                     // Password is incorrect
                     Toast.makeText(this, "Hibás email vagy jelszó", Toast.LENGTH_SHORT).show()
@@ -75,4 +90,21 @@ class loginpage_activity : AppCompatActivity() {
         val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
         return bytes.joinToString("") { "%02x".format(it) }
     }
+
+    private fun navigateToDashboard(userName: String) {
+        val intent = Intent(this@loginpage_activity, dashboard_activity::class.java)
+        intent.putExtra("USERNAME", userName)
+        startActivity(intent)
+    }
+
+    private fun decrypt(data: String, secret: String): String {
+        val cipher = Cipher.getInstance("AES")
+        val secretKeySpec = SecretKeySpec(secret.toByteArray(), "AES")
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec)
+        val decodedValue = Base64.decode(data, Base64.DEFAULT)
+        val decryptedValue = cipher.doFinal(decodedValue)
+        return String(decryptedValue)
+    }
+
+
 }
